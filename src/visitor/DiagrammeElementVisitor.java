dc745c335b993fa3425afed624faf9d913718baf
thaianthantrong <thaianthantrong@dhcp-2-9.gde.emn.fr>
2016-11-21 15:55:04 +0100

    Ajout des methodes dans l'interface

--- src/visitor/DiagrammeElementVisitor.java
+++ src/visitor/DiagrammeElementVisitor.java
@@ -4,5 +4,7 @@ public interface DiagrammeElementVisitor {
 
 	void visit(Diagram) ;
 	void visit(Type) ;
-	void visit(Arrow);
+	void visit(Arrow) ;
+	void visit(Method) ;
+	void visit(Variable) ;
 }
